a4c53fc92cde91886b032c9ddf0d3350f2f1dc09
DEVELOPERBibek10 <bibekpyakurel7@gmail.com>
2025-11-07 20:45:45 +0545

    ActiveProject component complete.

--- src/components/ActiveProject.tsx
+++ src/components/ActiveProject.tsx
@@ -12,35 +12,78 @@ import { useEffect, useMemo, useState } from "react";
 
 import ProjectsDropdown from "./ProjectsDropdown";
 import TaskStatusChart from "./TaskStatusChart";
+
 const ActiveProject = () => {
   const projects = useSelector((state: RootState) => state.kanban);
   const activeId = useSelector((state: RootState) => state.active.id);
   const project = useSelector(selectProjectById(activeId!));
   const dispatch = useDispatch();
   const [open, setOpen] = useState(false);
-  
+  const states = ["Total", "In Progress", "Waiting", "Completed"];
 
-   const completionPercentage = useMemo(() => {
-     if (!project) return null;
+  const OverallTasksStates = (state: string) => {
+    
+    switch (state) {
+      case "Total": {
+        const columns = ["todo", "inProgress", "done"];
+        let total = 0;
+        for (const col of columns) {
+          for (const proj of projects) {
+            total += proj.columns[col].tasks.length;
+          }
+        }
+        return total;
+      }
 
-     const totalTasks =
-       project.columns["todo"].tasks.length +
-       project.columns["inProgress"].tasks.length +
-       project.columns["done"].tasks.length;
+      case "In Progress": {
+        let inProgress = 0;
+        for (const proj of projects) {
+          inProgress += proj.columns["inProgress"].tasks.length;
+        }
+        return inProgress;
+      }
 
-     if (totalTasks === 0) return 0;
+      case "Waiting": {
+        let todo = 0;
+        for (const proj of projects) {
+          todo += proj.columns["todo"].tasks.length;
+        }
+        return todo;
+      }
 
-     return Math.round(
-       (project.columns["done"].tasks.length / totalTasks) * 100
-     );
-   }, [project]);
+      case "Completed": {
+        let done = 0;
+        for (const proj of projects) {
+          done += proj.columns["done"].tasks.length;
+        }
+        return done;
+      }
+
+      default:
+        return 0;
+    }
+  };
+
+  const completionPercentage = useMemo(() => {
+    if (!project) return null;
+
+    const totalTasks =
+      project.columns["todo"].tasks.length +
+      project.columns["inProgress"].tasks.length +
+      project.columns["done"].tasks.length;
+
+    if (totalTasks === 0) return 0;
+
+    return Math.round(
+      (project.columns["done"].tasks.length / totalTasks) * 100
+    );
+  }, [project]);
 
   useEffect(() => {
     if (projects.length === 1) {
       dispatch(setActiveProjectId({ id: projects[0].id }));
     }
   }, [dispatch, projects]);
-
 
   return (
     <>
@@ -79,54 +122,21 @@ const ActiveProject = () => {
         <div className="flex flex-col gap-2 w-full p-4">
           <span className="font-bold text-xl">Tasks</span>
           <div className="grid grid-cols-2 gap-3 mt-3">
-            <div className="p-3 bg-gray-100 rounded-xl">
-              <span className="text-gray-600 text-[12px]">TOTAL</span>
-              <div className="flex gap-2 mt-1 items-center">
-                <div
-                  data-orientation="vertical"
-                  aria-orientation="vertical"
-                  role="separator"
-                  className="w-1 h-4 bg-primary"
-                />
-                <span className="font-bold text-lg">10</span>
+            {states.map((state) => (
+              <div className="p-3 bg-gray-100 dark:bg-zinc-900 rounded-xl">
+                <span className="text-gray-600 dark:text-gray-300 text-[12px]">{ state.toUpperCase() }</span>
+                <div className="flex gap-2 mt-1 items-center">
+                  <div
+                    data-orientation="vertical"
+                    aria-orientation="vertical"
+                    role="separator"
+                    className="w-1 h-4 bg-primary"
+                  />
+                 
+                  <span className="font-bold text-lg dark:text-gray-200">{ OverallTasksStates(state) }</span>
+                </div>
               </div>
-            </div>
-            <div className="p-3 bg-gray-100 rounded-xl">
-              <span className="text-gray-600 text-[12px]">IN PROGRESS</span>
-              <div className="flex gap-2 mt-1 items-center">
-                <div
-                  data-orientation="vertical"
-                  aria-orientation="vertical"
-                  role="separator"
-                  className="w-1 h-4 bg-primary"
-                />  
-                <span className="font-bold text-lg">5</span>
-              </div>
-            </div>
-            <div className="p-3 bg-gray-100 rounded-xl">
-              <span className="text-gray-600 text-[12px]">WAITING</span>
-              <div className="flex gap-2 mt-1 items-center">
-                <div
-                  data-orientation="vertical"
-                  aria-orientation="vertical"
-                  role="separator"
-                  className="w-1 h-4 bg-primary"
-                />
-                <span className="font-bold text-lg">4</span>
-              </div>
-            </div>
-            <div className="p-3 bg-gray-100 rounded-xl">
-              <span className="text-gray-600 text-[12px]">COMPLETED</span>
-              <div className="flex gap-2 mt-1 items-center">
-                <div
-                  data-orientation="vertical"
-                  aria-orientation="vertical"
-                  role="separator"
-                  className="w-1 h-4 bg-primary"
-                />
-                <span className="font-bold text-lg">1</span>
-              </div>
-            </div>
+            ))}
           </div>
         </div>
       </div>
